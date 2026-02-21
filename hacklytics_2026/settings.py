e8ea68947420359cd1cb5353e45857009836410f
sunnyrammy <109098651+sunnyrammy@users.noreply.github.com>
2026-02-21 13:11:41 -0500

    added tailwind, jupyter

--- hacklytics_2026/settings.py
+++ hacklytics_2026/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #... DEFAULT_APPS ...
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'tailwind',
+    
+    
+    #... APPS ...
+    'app.users',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+TAILWIND_APP_NAME = 'theme'
